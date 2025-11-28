a82d9ed70b92e13b514c259d1668a21c0e121116
michukow <michal.kowal.bielsko@gmail.com>
2025-11-28 13:31:09 +0100

    Remove all files

--- odd_even.py
+++
@@ -1,4 +0,0 @@
-liczby = [1, 4, 7, 10, 13, 16, 21, 28]
-for i in liczby:
-	if i%2==0:
-		print(i)
--- opening_files.py
+++
@@ -1,7 +0,0 @@
-def file_operations(cos):
-	with open (cos,'r') as file:
-		txt = file.readlines()
-	return len(txt)
-
-print(file_operations("test.txt"))
-#i don't know if it works
